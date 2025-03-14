6768bb2c8cdfc9c9d660d60f347f447adaa5c7e9
BikashPandeya <bikashpandeya5@gmail.com>
2025-03-14 23:32:33 +0545

    rapid api integration

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,37 @@
+import axios from "axios"
+// import dotenv from "dotenv"
+
+// dotenv.config()
+
+const BASE_URL ='https://youtube-v31.p.rapidapi.com' 
+
+const options = {
+    params: {
+      maxResults: '50'
+    },
+    headers: {
+      'x-rapidapi-key': '0743b3629dmsh4a1e56da7159109p1c98a1jsne71bcb30567b',
+      'x-rapidapi-host': 'youtube-v31.p.rapidapi.com'
+    }
+  };
+
+  export const fetchFromAPI = async (url) => {
+    const {data} = await axios.get(`${BASE_URL}/${url}`, options)
+    return data
+  }
+
+
+// const options = {
+//     method: 'GET',
+//     url: 'https://youtube-v31.p.rapidapi.com/search',
+//     params: {
+//       relatedToVideoId: '7ghhRHRP6t4',
+//       part: 'id,snippet',
+//       type: 'video',
+//       maxResults: '50'
+//     },
+//     headers: {
+//       'x-rapidapi-key': '0743b3629dmsh4a1e56da7159109p1c98a1jsne71bcb30567b',
+//       'x-rapidapi-host': 'youtube-v31.p.rapidapi.com'
+//     }
+//   };
